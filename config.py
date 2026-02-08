d923b2794a06f97bbd1cd603fdad7eef2db1cc54
Versh <ikumbar59@gmail.com>
2026-02-08 10:15:56 +0530

    Fix ModuleNotFoundError: Use psycopg2-binary

--- config.py
+++ config.py
@@ -3,7 +3,11 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-CHANGE-IN-PROD'
     # Defaulting to root with empty password for local dev ease, can be overridden
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://root:2003@localhost/enrollment_db'
+    database_url = os.environ.get('DATABASE_URL')
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = database_url or 'mysql+mysqlconnector://root:2003@localhost/enrollment_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Email Config
